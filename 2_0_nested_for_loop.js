ed32e28facd1482f207e9bc987602b79b187eaca
nasirabbaskhan <akhtarabbas.islamicreaction@gmail.com>
2024-01-30 00:24:43 -0800

    adding more logic

--- 2_0_nested_for_loop.js
+++ 2_0_nested_for_loop.js
@@ -1,15 +1,22 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 let firstName = ["BlueRay ", "Upchuck ", "Lojack ", "Gizmo ", "Do-Rag "];
 let lastName = ["Zzz", "Burp", "Dogbone", "Droop"];
 let fullName = [];
-for (let i = 0; i < firstName.length; i++) {
-    for (let j = 0; j < lastName.length; j++) {
-        fullName.push(firstName[i] + lastName[j]);
-    }
-}
-console.log("Here is Array of FullName ");
-fullName.map((element, index) => {
-    console.log(index + 1, element);
+// for (let i = 0; i < firstName.length; i++) {
+//   for (let j = 0; j < lastName.length; j++) {
+//     fullName.push(firstName[i] + lastName[j]);
+//   }
+// }
+// console.log("Here is Array of FullName ");
+// fullName.map((element, index) => {
+//   console.log(index + 1, element);
+// });
+firstName.forEach((element1) => {
+    lastName.forEach((element2) => {
+        fullName.push(element1 + element2);
+    });
 });
-console.log("nasir");
+fullName.forEach((element) => {
+    console.log(element);
+});
+console.log(fullName);
+export {};
